e7feab47d3bcfe1b80ca793a2ca614aac55f8660
Yaroslav Krivushenko <epictruefake@gmail.com>
2021-02-07 20:48:37 +0200

    Updates

---
+++ precache-manifest.0a0205e3d9480765cd289facdc3e0342.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "262a0e470944eb3a5c9b",
+    "url": "/ricknmorty/static/css/main.6defe957.chunk.css"
+  },
+  {
+    "revision": "262a0e470944eb3a5c9b",
+    "url": "/ricknmorty/static/js/main.8cf79f34.chunk.js"
+  },
+  {
+    "revision": "8cee2e5c07d0ad621fec",
+    "url": "/ricknmorty/static/js/runtime~main.de89aeb3.js"
+  },
+  {
+    "revision": "7f0704b191b26dbd9f3f",
+    "url": "/ricknmorty/static/js/2.4b8ee14e.chunk.js"
+  },
+  {
+    "revision": "41044daaf14c050c2a6cfe12a6af1a47",
+    "url": "/ricknmorty/static/media/spinner.41044daa.svg"
+  },
+  {
+    "revision": "825488f7859acb3fac46bae10607618a",
+    "url": "/ricknmorty/static/media/rickandmorty2.825488f7.png"
+  },
+  {
+    "revision": "a5ce10401f9c303f4a7afce33c3986f7",
+    "url": "/ricknmorty/index.html"
+  }
+];
